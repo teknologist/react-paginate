53c88206bd8fd4ca4d61560c670d321c8c421aff
Eric Taïeb Walch <eric@teknologism.com>
2016-05-10 11:53:19 +0200

    break fix

--- react_components/BreakView.js
+++ react_components/BreakView.js
@@ -4,10 +4,20 @@ import React from 'react';
 
 export default class BreakView extends React.Component {
   render() {
+  	  let linkClassName = this.props.pageLinkClassName;
+
+   
+      if (typeof(linkClassName) !== 'undefined') {
+        linkClassName = linkClassName + ' ' + 'break';
+      } else {
+        linkClassName = 'break';
+      }
+  
     return (
-       <a {...this.props} href="" className={this.props.pageLinkClassName}>
+       <a {...this.props} href="" className={linkClassName}>
         {this.props.breakLabel}
       </a>
     );
   }
 };
+
